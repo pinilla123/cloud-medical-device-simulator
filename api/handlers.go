6defb1d588a87201de61a6fb58f5c02c3a1aac26
pinilla123 <nicolas.pinilla123@gmail.com>
2024-05-25 15:57:15 -0400

    Implemented GetData function in handlers.go for device vital signs retrieval and storage

--- api/handlers.go
+++ api/handlers.go
@@ -2,13 +2,27 @@ package api
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/pinilla123/cloud-medical-device-simulator/device"
 )
 
 func GetData(w http.ResponseWriter, r *http.Request) {
-	data := device.GenerateVitalSigns()
+	deviceID := r.URL.Query().Get("device_id")
+	if deviceID == "" {
+		http.Error(w, "device_id is required", http.StatusBadRequest)
+		return
+	}
+
+	data := device.GenerateVitalSigns(deviceID)
+	err := device.SaveVitalSigns(data)
+	if err != nil {
+		log.Printf("Failed to save data: %v", err)
+		http.Error(w, "Failed to save data", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
