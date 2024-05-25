62e91f33df7d3b36b02b3358786829fd145b4e87
pinilla123 <nicolas.pinilla123@gmail.com>
2024-05-25 15:53:34 -0400

    Added field(DeviceID) to the vital signs struct added (deviceID string) parameter to the GenerateVitalSigns function GenerateVitalSigns now returns the deviceID as well

--- device/simulator.go
+++ device/simulator.go
@@ -7,17 +7,19 @@ import (
 )
 
 type VitalSigns struct {
+	DeviceID      string
 	HeartRate     int
 	BloodPressure string
 }
 
-func GenerateVitalSigns() VitalSigns {
+func GenerateVitalSigns(deviceID string) VitalSigns {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	heartRate := r.Intn(40) + 60 // Random heart rate between 60 and 100
 	systolic := r.Intn(40) + 90  // Random systolic BP between 90 and 130
 	diastolic := r.Intn(20) + 60 // Random diastolic BP between 60 and 80
 
 	return VitalSigns{
+		DeviceID:      deviceID,
 		HeartRate:     heartRate,
 		BloodPressure: fmt.Sprintf("%d/%d", systolic, diastolic),
 	}
