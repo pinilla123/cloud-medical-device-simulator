37b36c9774ff983b2d2908cc232fa5da19c09185
pinilla123 <nicolas.pinilla123@gmail.com>
2024-05-25 15:48:20 -0400

    Added DynamoDB integration to save vital signs data Initialize DyanmoDB client using AWS SDK for go Implemented SaveVitalSigns function to store vital signs in DynamoDB

---
+++ device/storage.go
@@ -0,0 +1,43 @@
+package device
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+var svc *dynamodb.Client
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	svc = dynamodb.NewFromConfig(cfg)
+}
+
+func SaveVitalSigns(vs VitalSigns) error {
+	av, err := attributevalue.MarshalMap(vs)
+	if err != nil {
+		log.Printf("Failed to marshal vital signs: %v", err)
+		return err
+	}
+
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String("MedicalDeviceData"),
+		Item:      av,
+	}
+
+	_, err = svc.PutItem(context.TODO(), input)
+	if err != nil {
+		log.Printf("Failed to put item in DynamoDB: %v", err)
+		return err
+	}
+
+	return nil
+}
