dd31ec22233d9eef5b73c8894211d0304647d546
pinilla123 <nicolas.pinilla123@gmail.com>
2024-05-26 19:08:46 -0400

    Remove duplicate main.go and retain version in src/

--- src/main.go
+++ src/main.go
@@ -4,10 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/pinilla123/cloud-medical-device-simulator/api"
 )
 
 func main() {
+	lambda.Start(api.GetData)
+
 	http.HandleFunc("/data", api.GetData)
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
