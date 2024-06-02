9449f72b66fe9d0076e6122ff3fff6044a1419b0
pinilla123 <nicolas.pinilla123@gmail.com>
2024-06-01 20:52:32 -0400

    Added comment for testing purposes

--- src/device/storage.go
+++ src/device/storage.go
@@ -10,7 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
-var svc *dynamodb.Client
+var svc *dynamodb.Client // DynamoDB client
 
 func init() {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
