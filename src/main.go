42c6532b0484a4f2069730b45e46bbcc7c7e3045
pinilla123 <nicolas.pinilla123@gmail.com>
2024-06-01 20:51:35 -0400

    Refactored main.go to start AWS Lambda function and HTTP server for handling medical device data

--- src/main.go
+++ src/main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/pinilla123/cloud-medical-device-simulator/api"
+	"github.com/pinilla123/cloud-medical-device-simulator/src/api"
 )
 
 func main() {
-	lambda.Start(api.GetData)
+	lambda.Start(api.LambdaHandler)
 
-	http.HandleFunc("/data", api.GetData)
+	http.HandleFunc("/data", api.HTTPHandler)
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
